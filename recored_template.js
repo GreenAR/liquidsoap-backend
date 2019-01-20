fb241d114f931ef731ebe621275a78cbfbc257c2
GreenArbi <grn.arb@gmail.com>
2019-01-20 15:04:32 +0100

    add recording feature

---
+++ recored_template.js
@@ -0,0 +1,70 @@
+let fs, http, port, server, websocket, wsServer;
+
+fs = require("fs");
+const key  = fs.readFileSync('/home/liquser/privkey.pem', 'utf8');
+const cert = fs.readFileSync('/home/liquser/fullchain.pem', 'utf8');
+http = require("https");
+
+websocket = require("websocket");
+
+port = 10004;
+
+server = http.createServer({key, cert},function(req, res) {
+    console.log((new Date) + " -- Received request for " + req.url);
+    res.writeHead(404);
+    return res.end();
+});
+
+server.listen(port, function() {
+    return console.log((new Date) + " -- Server ready on port " + port);
+});
+
+wsServer = new websocket.server({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+wsServer.on("request", function(req) {
+    let connection, fd;
+    fd = null;
+    console.log((new Date) + " -- Connection from " + req.origin);
+    connection = req.accept("webcast", req.origin);
+    console.log((new Date) + " -- Connection accepted");
+    return connection.on("message", function(msg) {
+        let ext;
+        if (msg.type === "utf8") {
+            msg.utf8Data = JSON.parse(msg.utf8Data);
+        }
+        if (connection.hello == null) {
+            if (msg.type !== "utf8" || msg.utf8Data.type !== "hello") {
+                console.log((new Date) + " -- Error: first message not hello!");
+                return connection.close();
+            }
+            connection.hello = msg.utf8Data.data;
+            console.log((new Date) + " -- Mount point: " + req.httpRequest.url + ".");
+            console.log((new Date) + " -- MIME type: " + connection.hello.mime + ".");
+            console.log((new Date) + " -- Audio channels: " + connection.hello.audio.channels + ".");
+            if (connection.hello.mime === "audio/mpeg") {
+                console.log((new Date) + " -- Audio bitrate: " + connection.hello.audio.bitrate + ".");
+            }
+            ext = connection.hello.mime === "audio/mpeg" ? "mp3" : "raw";
+            fd = fs.openSync("src/record/ifm.firstwebradio.com/15/" + Date.now()+ "."+ ext, "w");
+            return;
+        }
+        switch (msg.type) {
+            case "utf8":
+                switch (msg.utf8Data.type) {
+                    case "metadata":
+                        return console.log((new Date) + " -- Got new metadata: " + (JSON.stringify(msg.utf8Data.data)));
+                    default:
+                        return console.log((new Date) + " -- Invalid message");
+                }
+                break;
+            case "binary":
+                console.log((new Date) + " -- Got " + msg.binaryData.length + " bytes of binary data");
+                return fs.writeSync(fd, msg.binaryData, 0, msg.binaryData.length);
+            default:
+                return console.log((new Date) + " -- Invalid message");
+        }
+    });
+});
